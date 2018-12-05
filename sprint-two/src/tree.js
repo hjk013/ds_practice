fc640ea1ff08e89adb85e19c24790e95d0148c54
Hyeon Jun Kim <hjk013@gmail.com>
2018-12-04 21:04:20 -0800

    Tree Completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
-var Tree = function(value) {
-  var newTree = {};
+const Tree = function(value) {
+  const newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +12,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  for (let i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    }
+    return false;
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// need to figure out how this works for nested children
+// how does it iterate?
+// let includes = false;
+
+// let findTarget = obj => {
+//   // console.log(obj.value === this.children[i].value)
+//   if (obj.value === target) {
+//     includes = true;
+//   } else {
+//     obj.children.forEach(child => {
+//       findTarget(child);
+//     })
+//   }
+// }
+// findTarget(this)
+// return includes;
